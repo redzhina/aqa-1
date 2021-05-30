a7daeb7c83d3fa12af618254ef3686a44c3f75fc
Regina Shakirova <regina.shak@yandex.ru>
2021-05-30 16:07:42 +0300

    init two

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,30 +1,30 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
-    public void shouldRemainBelowBoundary() {
+    @org.junit.Test
+    public void shouldRemainBelowBoundaryJunit() {
         int purchase = 900;
         assertEquals(service.remain(purchase), 100);
     }
 
-    @org.testng.annotations.Test
-    public void shouldRemainAsBoundaryIfZero() {
+    @org.junit.Test
+    public void shouldRemainAsBoundaryIfZeroJunit() {
         int purchase = 0;
         assertEquals(service.remain(purchase), 1000);
     }
 
-    @org.testng.annotations.Test
-    public void shouldBeRemainderIfAboveBoundary() {
+    @org.junit.Test
+    public void shouldBeRemainderIfAboveBoundaryJunit() {
         int purchase = 1300;
         assertEquals(service.remain(purchase), 700);
     }
 
-    @org.testng.annotations.Test
-    public void shouldBeNoRemainderIfBoundary() {
+    @org.junit.Test
+    public void shouldBeNoRemainderIfBoundaryJunit() {
         int purchase = 1000;
         assertEquals(service.remain(purchase), 0);
     }
